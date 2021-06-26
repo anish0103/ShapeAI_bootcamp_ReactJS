5fc820a6357799873663196ebe17757e9b043c2c
Anish Patel <85151048+anish0103@users.noreply.github.com>
2021-06-26 17:43:45 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Upcoming Bootcamps</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>React Js Bootcamp</h1>
+      <p>Learn some new Technology in 7 days.</p>
+    </div>
+  );
+}
+
+export default Info;
